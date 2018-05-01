a328745634eca48e3eec2094f10e0040117d83a1
rogercodes1 <roger.ucsb@gmail.com>
2018-05-01 19:07:42 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,24 +1,21 @@
 class Post < ActiveRecord::Base
-
   belongs_to :author
-  validate :is_title_case
+    validate :is_title_case
+
+    # before_validation :make_title_case
 
   # New Code!!
-  # before_save :make_title_case
-  before_validation :make_title_case
-  # new code after explaining what before_save means
-  before_save :email_author_about_post
+    # before_save :email_author_about_post
 
+    private
 
-  private
+    def is_title_case
+      if title.split.any?{|w|w[0].upcase != w[0]}
+        errors.add(:title, "Title must be in title case")
+      end
+    end
 
-  def is_title_case
-    if title.split.any?{|w|w[0].upcase != w[0]}
-      errors.add(:title, "Title must be in title case")
+    def make_title_case
+      self.title = self.title.titlecase
     end
   end
-
-  def make_title_case
-    self.title = self.title.titlecase
-  end
-end
